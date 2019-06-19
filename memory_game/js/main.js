182f81e009c7328bdcc8ab3d6fc45342c6825076
Nilana Rocha <nilanaarquitetura@gmail.com>
2019-06-19 21:36:39 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,16 +24,36 @@ var cards = [
 ];
 var cardsInPlay = [];
 
+function createBoard() {
+  var ul = document.querySelector("#game-board");
+  ul.innerHTML = "";
+
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    ul.appendChild(cardElement);
+  }
+
+
+}
+
 function checkForMatch() {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
+  if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
     alert("You found a match!");
   } else {
     alert("Sorry, try again.");
   }
+  cardsInPlay = [];
+  createBoard();
+
 }
 
-function flipCard(cardId) {
+function flipCard() {
+  var cardId = this.getAttribute('data-id');
   var selectedCard = cards[cardId];
+  this.setAttribute("src", selectedCard.cardImage);
 
   console.log("User flipped " + selectedCard.rank);
   console.log(selectedCard.cardImage);
@@ -46,5 +66,4 @@ function flipCard(cardId) {
   }
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
